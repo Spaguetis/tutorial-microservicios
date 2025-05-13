72e65db4dcc1e1a71732fd4712208d67ff38fed8
Spaguetis <marioandux123@gmail.com>
2025-05-12 20:10:01 -0400

    v2

--- javascript.js/registro rapido y funcional js/index.js
+++ javascript.js/registro rapido y funcional js/index.js
@@ -1,8 +1,19 @@
 const express = require("express");
-
+const mysql = require("mysql")
 const app = express();
 
+let conexion = mysql.createConnection({
+    host:"localhost",
+    database:"tutorial_1_microservicio",
+    user:"root",
+    password:"Mayito16",
+})
+
+
 app.set("view engine", "ejs");
+
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
 
 app.get("/", function(req, res) {
     res.render("register");
@@ -16,10 +27,24 @@ app.get("/", function(req, res) {
 app.post("/validar", function(req,res){
     const data = req.body;
 
-    console.log(data);
+    let cedula = data.rut;
+    let name = data.nombre;
+    let lastname = data.primer_apellido;
+    let Direccion = data.direccion;
+    let Correo = data.correo_electronico;
+    let Key = data.pass;
+
+    let register = "INSERT INTO informacion_cliente (RUT,nombre,primer_apellido,direccion,correo_electronico,password) VALUES('"+cedula+"','"+name+"','"+lastname+"','"+Direccion+"','"+Correo+"','"+Key+"')";
+    
+    conexion.query(register, function(error){
+        if(error){
+            throw error;
+        }else{
+            console.log("datos almacenados");
+        }
+    });
 })
 
 app.listen(3000, function() {
     console.log("servidor creado http://localhost:3000");
 });
-
