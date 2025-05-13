c94afc2c1279e2fb26469988e5e80d062ba8d352
Spaguetis <marioandux123@gmail.com>
2025-05-12 21:05:18 -0400

    Create turorial2_data insert.sql

---
+++ scripts/SQL/mysql/turorial2_data insert.sql
@@ -0,0 +1,122 @@
+CREATE DATABASE  IF NOT EXISTS `tutorial_1_ microservicio` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `tutorial_1_ microservicio`;
+-- MySQL dump 10.13  Distrib 8.0.41, for Win64 (x86_64)
+--
+-- Host: localhost    Database: tutorial_1_ microservicio
+-- ------------------------------------------------------
+-- Server version	9.3.0
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `informacion_cliente`
+--
+
+DROP TABLE IF EXISTS `informacion_cliente`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `informacion_cliente` (
+  `RUT` int NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `primer_apellido` varchar(100) NOT NULL,
+  `direccion` varchar(150) NOT NULL,
+  `correo_electronico` varchar(45) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  PRIMARY KEY (`RUT`),
+  UNIQUE KEY `RUT_UNIQUE` (`RUT`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `informacion_cliente`
+--
+
+LOCK TABLES `informacion_cliente` WRITE;
+/*!40000 ALTER TABLE `informacion_cliente` DISABLE KEYS */;
+INSERT INTO `informacion_cliente` VALUES (1,'mario','mieres','tarapaca 890 santiago cento region metropolitana','marioandux123@gmail.com','Mayito16');
+/*!40000 ALTER TABLE `informacion_cliente` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `mascotas_clientes`
+--
+
+DROP TABLE IF EXISTS `mascotas_clientes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `mascotas_clientes` (
+  `#chip` int NOT NULL,
+  `especie` varchar(50) NOT NULL,
+  `raza` varchar(50) NOT NULL,
+  `peso` int NOT NULL,
+  `altura` int NOT NULL,
+  `RUT_cliente` int NOT NULL,
+  PRIMARY KEY (`#chip`),
+  KEY `rut cliente_idx` (`RUT_cliente`),
+  CONSTRAINT `rut cliente` FOREIGN KEY (`RUT_cliente`) REFERENCES `informacion_cliente` (`RUT`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `mascotas_clientes`
+--
+
+LOCK TABLES `mascotas_clientes` WRITE;
+/*!40000 ALTER TABLE `mascotas_clientes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `mascotas_clientes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `stock`
+--
+
+DROP TABLE IF EXISTS `stock`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `stock` (
+  `id_producto` int NOT NULL,
+  `descripcion` varchar(150) NOT NULL,
+  `marca` varchar(50) NOT NULL,
+  `modelo` varchar(45) NOT NULL,
+  `precio` decimal(10,2) NOT NULL,
+  `stock` int NOT NULL,
+  PRIMARY KEY (`id_producto`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `stock`
+--
+
+LOCK TABLES `stock` WRITE;
+/*!40000 ALTER TABLE `stock` DISABLE KEYS */;
+/*!40000 ALTER TABLE `stock` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping events for database 'tutorial_1_ microservicio'
+--
+
+--
+-- Dumping routines for database 'tutorial_1_ microservicio'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-05-12 20:44:17
