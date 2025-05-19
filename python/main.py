32111bb6fcc9667d913e5dc9f23f69f6f1c06243
Spaguetis <marioandux123@gmail.com>
2025-05-19 18:12:04 -0400

    comiezo del proyecto aun armando los scrpts de back y front

--- python/main.py
+++ python/main.py
@@ -1,10 +1,23 @@
-from flask import Flask,jsonify, request
+from fastapi import FastAPI, Form, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import RedirectResponse
 
+app = FastAPI()
 
-app= Flask(__name__)
-if __name__=='__main__':
-    app.run(debug=True)
+# Directorios para HTML y archivos estáticos
+templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
-@app.route('/')
-def root():
-    return "root"
+@app.get("/", response_class=HTMLResponse)
+def mostrar_formulario(request: Request):
+    return templates.TemplateResponse("register.html", {"request": request})
+
+@app.post("/registro")
+async def registrar_usuario(
+    request: Request,
+    user: str = Form(...),
+    email: str = Form(...),
+    password: str = Form(...)
+)
